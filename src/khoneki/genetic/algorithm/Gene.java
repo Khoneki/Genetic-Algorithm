215aa2d8d58e9fb2a29290db67a33cc8f52a1671
일가크 <woni8708@naver.com>
2015-11-06 18:20:57 +0900

    finished make method

--- src/khoneki/genetic/algorithm/Gene.java
+++ src/khoneki/genetic/algorithm/Gene.java
@@ -5,11 +5,40 @@ package khoneki.genetic.algorithm;
  * @since 2015-11-05
  */
 public class Gene {
-    public byte gene;
+    public int gene[];
     public int score;
-    public int fit;
+    public double fit;
 
-    public void setGene() {}
-    public void getScore() {}
-    public void mutent() {}
+    public void setGene() {
+        for(int i = 0; i < 10; i++) {
+            this.gene[i] =  Math.floor(Math.random()*2)==0?0:1;
+        }
+    }
+    public void getScore() {
+        score = 0;
+        int imaginary[] = {0,0,0,0,0,1,1,1,1,1};
+        for(int i = 0; i < 10; i++) {
+            if(gene[i] == imaginary[i]) score++;
+        }
+    }
+    public void mutent() {
+        this.getScore();
+        int a = (int)Math.floor(Math.random()*100);
+        if(a <= (10-score)*(10-score)) {
+            for(int i = 0; i < 10; i++) {
+                switch(gene[i]) {
+                    case 0:
+                        gene[i] = 1;
+                        break;
+                    case 1:
+                        gene[i] = 0;
+                        break;
+                }
+            }
+        }
+    }
+    @Override
+    public Gene clone() {
+        return this.clone();
+    }
 }
--- src/khoneki/genetic/algorithm/Population.java
+++ src/khoneki/genetic/algorithm/Population.java
@@ -6,16 +6,59 @@ package khoneki.genetic.algorithm;
  */
 public class Population {
     public int entCnt;
-    public Gene entity;
+    public Gene entity[];
     public int max = 0, min = 10, sumOfFit = 0;
 
-    public void regen() {}
-    public void getMax() {}
-    public void getMin() {}
+    public void regen() {
+        for(int i = 0; i < entCnt; i++) {
+            entity[i].setGene();
+        }
+    }
+    public void getMax() {
+        for(int i = 0; i < entCnt; i ++) {
+            if(entity[i].score > max) max = entity[i].score;
+        }
+    }
+    public void getMin() {
+        for(int i = 0; i < entCnt; i ++) {
+            if(entity[i].score < max) min = entity[i].score;
+        }
+    }
     public double getAverage() {
+        int average = 0;
+        for(int i = 0; i < entCnt; i++) {
+            average += entity[i].score;
+        }
+        return average;
+    }
+    public void f() {
+        for(int i = 0; i < entCnt; i++)
+            entity[i].fit = max-entity[i].score + (max-min)/(4-1);
+    }
+    public void getSumOfFit() {
+        sumOfFit = 0;
+        for(int i = 0; i < entCnt; i++)
+            sumOfFit += entity[i].fit;
+    }
+    public int rouletteWheel() {
+        double sum = 0;
+        int a = (int)Math.floor(Math.random()*sumOfFit);
+        for(int i = 0; i < entCnt; i++) {
+            sum += entity[i].fit;
+            if(a<sum) return i;
+        }
         return 0;
     }
-    public void f() {}
-    public void getSumOfFit() {}
-    public void rouletteWheel() {}
+    public void sort() {
+        Gene temp;
+        for(int i = 0; i < entCnt-2; i++) {
+            for(int m = i; m < entCnt-1; m++) {
+                if(entity[m].score>entity[m+1].score) {
+                    temp = entity[m].clone();
+                    entity[m+1] = entity[m].clone();
+                    entity[m+1] = temp.clone();
+                }
+            }
+        }
+    }
 }
